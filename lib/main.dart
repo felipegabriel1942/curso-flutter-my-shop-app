813e27ced22b77589fc3792a428a647fae47a9cd
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2019-12-23 12:27:51 -0300

    Tela de carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './screens/cart_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
           home: ProductsOverviewScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen()
           }),
     );
   }
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../screens/cart_screen.dart';
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
@@ -43,8 +44,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                     child: ch,
                     value: cart.itemCount.toString(),
                   ),
-              child:
-                  IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {}))
+              child: IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CartScreen.routeName);
+                  }))
         ],
       ),
       body: ProductsGrid(_showOnlyFavorites),
