b3cbb117a7cfad828632bfbed0a16d355750d729
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2020-01-20 21:42:34 -0300

    Introduzindo formularios no projeto

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:curso_flutter_my_shop_app/screens/user_products_screen.dart';
 import 'package:flutter/material.dart';
 import '../screens/orders_screen.dart';
 
@@ -26,7 +27,15 @@ class AppDrawer extends StatelessWidget {
               Navigator.of(context)
                   .pushReplacementNamed(OrdersScreen.routeName);
             },
-          )
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
+            },
+          ),
         ],
       ),
     );
---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String title;
+  final String imagemUrl;
+
+  UserProductItem(this.title, this.imagemUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imagemUrl),
+      ),
+      trailing: Container(
+        width: 100,
+        child: Row(children: <Widget>[
+          IconButton(icon: Icon(Icons.edit), onPressed: () {}, color: Theme.of(context).primaryColor),
+          IconButton(icon: Icon(Icons.delete), onPressed: () {}, color: Theme.of(context).errorColor)
+        ],),
+      ),
+    );
+  }
+}
